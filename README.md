26eb487755f6f1b61ad70dda9972ff215944af5a
Michael Strobl <mstrobl@ualberta.ca>
2022-09-19 11:12:57 +0200

    readme and simple questions processor

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # KGQA using KG Construction Data
 
-Code and models coming soon.
+## Datasets
+
+Download all datasets and move them to data/
+
+### FREDA
+
+1. Download database from https://github.com/mjstrobl/FREDA/blob/main/database/main.tar.xz
+2. Extract (main.db should be in data/)
+
+### LC-QuaD 2.0
+
+1. Download data from https://github.com/AskNowQA/LC-QuAD2.0/tree/master/dataset
+2. Move test.json and train.json to data/
+
+### SimpleQuestions
+
+1. Download data from https://github.com/davidgolub/SimpleQA/tree/master/datasets/SimpleQuestions
+2. Move annotated_fb_data_{train|dev/|test}.txt to data/
+
+
+### Freebase to names
+
+1. Download Freebase Mid to names mapping: https://drive.google.com/file/d/1NM8Ti9qM_DB_m6OMREXfYubTisFsauCU/view?usp=sharing
+2. Move to data/
---
+++ src/processors/simple_questions_processor.py
@@ -0,0 +1,67 @@
+import json
+import re
+import glob, os
+
+RELATIONS = {"www.freebase.com/people/person/place_of_birth":0,
+             "www.freebase.com/people/person/nationality":1,
+             "www.freebase.com/location/location/people_born_here":0,
+             "www.freebase.com/people/person/children":3,
+             "www.freebase.com/people/person/parents":3,
+             "www.freebase.com/organization/organization/founders":2,
+             "www.freebase.com/organization/organization_founder/organizations_founded":2
+}
+
+INVERSE = {"www.freebase.com/location/location/people_born_here",
+           "www.freebase.com/people/person/children",
+           "www.freebase.com/organization/organization/founders"}
+
+mid2names = json.load(open("data/mid2names.json"))
+
+results = []
+
+for filename in ["test", "train", "valid"]:
+    with open('data/annotated_fb_data_' + filename + '.txt') as f:
+        for line in f:
+            line = line.strip()
+            parts = line.split('\t')
+
+            if len(parts) < 4:
+                continue
+
+            subject = parts[0][16:]
+            object = parts[2][16:]
+            question = parts[3]
+
+            relation = parts[1]
+            if relation not in RELATIONS:
+                continue
+
+            if subject in mid2names:
+                subject_names = mid2names[subject]
+                found = False
+                for subject_name in subject_names:
+                    if subject_name.lower() in question.lower():
+                        insensitive_hippo = re.compile(re.escape(subject_name), re.IGNORECASE)
+                        if relation in INVERSE:
+                            question = insensitive_hippo.sub("<e2>" + subject_name + "</e2>", question)
+                        else:
+                            question = insensitive_hippo.sub("<e1>" + subject_name + "</e1>", question)
+                        found = True
+                        actual_name = subject_name
+                        break
+                if found:
+                    results.append({"sentence1":question, "sentence2":"", "label":RELATIONS[relation]})
+
+                    if relation in INVERSE:
+                        question = question.replace("<e2>", "<e1>")
+                        question = question.replace("</e2>", "</e1>")
+                    else:
+                        question = question.replace("<e1>","<e2>")
+                        question = question.replace("</e1>", "</e2>")
+                    results.append({"sentence1": question, "sentence2": "", "label": 4})
+
+    with open('data/simple_' + filename + '.json', 'w') as f:
+        for o in results:
+            f.write(json.dumps(o) + "\n")
+
+
